b708ef250e1631c10e905345274213e0625f1927
Lukas Naruševičius <narusevic@gmail.com>
2017-09-12 18:51:59 +0300

    Renamed form

--- Foosball/Foosball/Video.Designer.cs
+++ Foosball/Foosball/Video.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Foosball
 {
-    partial class Form1
+    partial class Video
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Video
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(458, 336);
+            this.Name = "Video";
+            this.Text = "Video";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
