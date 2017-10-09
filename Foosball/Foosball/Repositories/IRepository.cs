b273b2e7cb79a1a8e1c3e83a47506d13d5f244d4
Lukas Naruševičius <narusevic@gmail.com>
2017-10-09 23:11:15 +0300

    Repos, model, simple data context

---
+++ Foosball/Foosball/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Foosball.Repositories
+{
+    public interface IRepository<T>
+    {
+        T Get(int id);
+
+        void Post(T entity);
+
+        void Update(int id, T entity);
+
+        void Remove(int id);
+    }
+}
---
+++ Foosball/Foosball/Repositories/MatchRepository.cs
@@ -0,0 +1,37 @@
+﻿using Foosball.DataAccess;
+using Foosball.Models;
+using System.Linq;
+
+namespace Foosball.Repositories
+{
+    public class MatchRepository : IRepository<Match>
+    {
+        public static MatchRepository Instance;
+        private readonly DataContext _dataContext;
+
+        public MatchRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public Match Get(int id)
+        {
+            return _dataContext.Matches.First(m => m.Id == id);
+        }
+
+        public void Post(Match match)
+        {
+            _dataContext.Matches.Add(match);
+        }
+
+        public void Update(int id, Match entity)
+        {
+
+        }
+
+        public void Remove(int id)
+        {
+            _dataContext.Matches.Remove(Get(id));
+        }
+    }
+}
