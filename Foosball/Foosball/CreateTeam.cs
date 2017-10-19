67005b9874a12c7250a6e480123a2c4f6092130b
Lukas Naruševičius <narusevic@gmail.com>
2017-10-20 01:21:01 +0300

    all managing forms

---
+++ Foosball/Foosball/CreateTeam.cs
@@ -0,0 +1,41 @@
+﻿using Foosball.Controllers;
+using System;
+using System.Windows.Forms;
+
+namespace Foosball
+{
+    public partial class CreateTeam : Form
+    {
+        ManagingController _managingController = new ManagingController();
+
+        public CreateTeam()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var teamName = tbTeamName.Text;
+            var playerAName = tbPlayerA.Text;
+            var playerBName = tbPlayerB.Text;
+
+            if (teamName != string.Empty || playerAName != string.Empty || playerBName != string.Empty)
+            {
+                if (_managingController.TeamExists(teamName) || _managingController.PlayersExists(playerAName, playerBName))
+                {
+                    MessageBox.Show("Player or team exists!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    _managingController.CreateTeam(teamName, playerAName, playerBName);
+
+                    Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Input all fields!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
--- Foosball/Foosball/Properties/Resources.Designer.cs
+++ Foosball/Foosball/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace Foosball.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap managing {
+            get {
+                object obj = ResourceManager.GetObject("managing", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Statistics {
             get {
                 object obj = ResourceManager.GetObject("Statistics", resourceCulture);
