cec135e7ca475abbd7b99f96dfc16b700ba37ee5
Lukas Naruševičius <narusevic@gmail.com>
2017-11-20 18:15:15 +0200

    Added missing files

---
+++ Foosball/FoosballWebsite/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FeedService } from './shared/services/feed.service';
+import { SignalRConnectionStatus } from './shared/interfaces';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/app.component.html',
+    providers: [FeedService]
+})
+export class AppComponent implements OnInit {
+
+    constructor(private service: FeedService) { }
+
+    ngOnInit() {
+        this.service.start(true).subscribe(
+            null,
+            error => console.log('Error on init: ' + error));
+    }
+}
