e24d72c1215414e271efe9dfb3c5ce21fc9e2cf6
Lukas Naruševičius <narusevic@gmail.com>
2017-11-21 14:59:14 +0200

    added website back

---
+++ Foosball/FoosballWebsite/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ConfigService } from './shared/services/config.service';
+import { DataService } from './shared/services/data.service';
+import { HomeComponent } from './home/home.component';
+import { HighlightDirective } from './shared/directives/highlight.directive';
+import { MatchComponent } from './shared/components/match.component';
+import { ChatComponent } from './shared/components/chat.component';
+import { routing } from './app.routes';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        ChatComponent,
+        HomeComponent,
+        HighlightDirective,
+        MatchComponent
+    ],
+    bootstrap: [AppComponent],
+    providers: [ 
+        ConfigService,
+        DataService
+    ]
+})
+export class AppModule { }
